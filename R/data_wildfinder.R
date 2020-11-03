9c7db103fe34ac6c30fdbd0369133d095a5db16a
Marine <marine.ballutaud@univ-lille.fr>
2020-11-03 15:08:40 +0100

    :whale: create function to read the data and modify description

---
+++ R/data_wildfinder.R
@@ -0,0 +1,39 @@
+
+#' Get data ecoregion
+#'
+#' @return data.frame with info on ecoregion
+#' @export
+#'
+#' @examples
+data_ecoregion <- function() { #pas de parametres
+  ob <- readr::read_csv(here::here("data", "wwf-wildfinder", "wildfinder-ecoregions_list.csv"))
+  #here donne le chemin du dataset, on est a la racine
+  #readr fabrique des tables
+  #read_csv pour les doc en csv
+}
+
+#' Get data mammals
+#'
+#' @return data.frame with info on mammals
+#' @export
+#'
+#' @examples
+data_ecoregion <- function() { #pas de parametres
+  ob <- readr::read_csv(here::here("data", "wwf-wildfinder", "wildfinder-mammals_list.csv"))
+  #here donne le chemin du dataset, on est a la racine
+  #readr fabrique des tables
+  #read_csv pour les doc en csv
+}
+
+#' Get data species
+#'
+#' @return data.frame with info on species
+#' @export
+#'
+#' @examples
+data_ecoregion <- function() { #pas de parametres
+  ob <- readr::read_csv(here::here("data", "wwf-wildfinder", "wildfinder-ecoregion_species.csv"))
+  #here donne le chemin du dataset, on est a la racine
+  #readr fabrique des tables
+  #read_csv pour les doc en csv
+}
