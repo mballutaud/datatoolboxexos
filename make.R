b848c8214cea54976731f8b50cb1d5398cba1bdb
Marine <marine.ballutaud@univ-lille.fr>
2020-11-04 15:32:26 +0100

    :boom: recovery operation!

--- make.R
+++ make.R
@@ -11,7 +11,18 @@ rm(list = ls())
 devtools::install_deps()
 
 # ----- install compendium package
-devtools::install(build = FALSE)
+devtools::install(build = FALSE) #build c'est comme quand install package: install, library and load
+devtools::load_all()
 
 # ----- Knit exo dplyr
 rmarkdown::render(here::here("exercices","exo_dplyr.Rmd"))
+
+# ----- Knit exo ggplot2
+rmarkdown::render(here::here("exercices","exo_ggplot2.Rmd"))
+
+# ----- drake
+## Execute plan
+drake::r_make()
+## Visualize
+drake::r_vis_drake_graph(targets_only = TRUE)
+drake::r_vis_drake_graph()
